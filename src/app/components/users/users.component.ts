50c9abf58565876f604cf01f81d4749cc2098efa
Tsyriulnikov <max_djvu@mail.ru>
2022-09-29 20:48:47 +0300

    users5

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {User, UsersService} from "../../services/users.service";
 import {Observable} from "rxjs";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-users',
@@ -11,11 +12,21 @@ export class UsersComponent implements OnInit {
   users$!: Observable<User[]>
 
   // constructor(private usersService: UsersService) {
-  constructor(private usersService: UsersService) {}
+  constructor(private usersService: UsersService, private route: ActivatedRoute) {
+  }
 
 
   ngOnInit(): void {
-   this.users$=this.usersService.getUsers()
+    this.getUsers(1)
   }
 
+  getUsers(page:number) {
+    this.users$ = this.usersService.getUsers(page)
+  }
+
+  nextUserHandler() {
+    const page = Number(this.route.snapshot.queryParamMap.get('page'))
+    const nextPage=page+1
+    this.getUsers(nextPage)
+  }
 }
